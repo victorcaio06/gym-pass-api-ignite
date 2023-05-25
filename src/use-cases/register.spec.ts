56687eb2c38fbed5499a159ab4ba8e4a05bb74b8
Victor Caio <kaioaraujo426@gmail.com>
2023-05-24 23:24:17 -0300

    TEST: added test to see if password is encrypted

---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest';
+import { RegisterUseCase } from './register';
+import { compare } from 'bcryptjs';
+
+describe('Register User Case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+      async findByEmail(email) {
+        return null;
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+});
