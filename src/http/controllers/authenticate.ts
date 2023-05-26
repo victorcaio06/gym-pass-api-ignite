dcb55eecb6c74bd11819d3a92c8a170c21f4d54e
Victor Caio <kaioaraujo426@gmail.com>
2023-05-26 13:35:17 -0300

    REFACTOR: added factory pattern

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -4,6 +4,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { AuthenticateUseCase } from '@/use-cases/authenticate';
 import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-erros';
+import { makeAuthenticateUseCase } from '@/use-cases/factories/make-authenticate-use-case';
 
 export async function authenticate(
   request: FastifyRequest,
@@ -17,9 +18,7 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
-    const prismaUsersRepository = new PrismaUsersRepository();
-
-    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository);
+    const authenticateUseCase = makeAuthenticateUseCase();
 
     await authenticateUseCase.execute({ email, password });
   } catch (error) {
