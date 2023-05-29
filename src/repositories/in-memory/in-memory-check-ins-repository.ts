5e1988f744dd1e9ebb9a547ec0ba20a459130e91
Victor Caio <kaioaraujo426@gmail.com>
2023-05-29 20:05:28 -0300

    FEAT: added test for check in minus location validation

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -2,11 +2,12 @@ import { CheckIn, Prisma } from '@prisma/client';
 import { randomUUID } from 'node:crypto';
 
 import { CheckInsRepository } from '../check-ins-repository';
+import dayjs from 'dayjs';
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public checkIns: CheckIn[] = [];
 
-  async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
+  async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
@@ -18,5 +19,22 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.checkIns.push(checkIn);
 
     return checkIn;
+  }
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const startOfDay = dayjs(date).startOf('date');
+    const endOfDay = dayjs(date).endOf('date');
+
+    const checkInOnSameDate = this.checkIns.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at);
+      const inSameDate =
+        checkInDate.isAfter(startOfDay) && checkInDate.isBefore(endOfDay);
+
+      return checkIn.user_id === userId && inSameDate;
+    });
+
+    if (!checkInOnSameDate) return null;
+
+    return checkInOnSameDate;
   }
 }
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,21 +1,84 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository';
-import { hash } from 'bcryptjs';
 import { CheckInUseCase } from './check-in';
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository';
+import { Decimal } from '@prisma/client/runtime/library';
 
 let checkInsRepository: InMemoryCheckInsRepository;
+let gymsRepository: InMemoryGymsRepository;
 let sut: CheckInUseCase;
 
 describe('Check in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository();
-    sut = new CheckInUseCase(checkInsRepository);
+    gymsRepository = new InMemoryGymsRepository();
+    sut = new CheckInUseCase(checkInsRepository, gymsRepository);
+
+    gymsRepository.gyms.push({
+      id: 'gym-id-test',
+      title: 'Academia FF',
+      description: 'Seu objetivo nossa meta',
+      phone: '8888888888',
+      latitude: new Decimal(0),
+      longitude: new Decimal(0),
+    });
+
+    vi.useFakeTimers();
   });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
   it('should be able to check in', async () => {
-    const checkIn = await checkInsRepository.create({
-      gym_id: 'gym-id-test',
-      user_id: 'user-id-test',
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id-test',
+      userId: 'user-id-test',
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: 'gym-id-test',
+      userId: 'user-id-test',
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    await expect(async () => {
+      await sut.execute({
+        gymId: 'gym-id-test',
+        userId: 'user-id-test',
+        userLatitude: 0,
+        userLongitude: 0,
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: 'gym-id-test',
+      userId: 'user-id-test',
+      userLatitude: 0,
+      userLongitude: 0,
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id-test',
+      userId: 'user-id-test',
+      userLatitude: 0,
+      userLongitude: 0,
     });
 
     expect(checkIn.id).toEqual(expect.any(String));
