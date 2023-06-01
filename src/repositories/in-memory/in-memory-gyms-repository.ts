ea4a9209ff1077d4eb1c6993d11dd359c7ee28be
Victor Caio <kaioaraujo426@gmail.com>
2023-06-01 16:55:57 -0300

    FEAT: added create gym use case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 import { GymsRepository } from '../gyms-repository';
+import { randomUUID } from 'crypto';
 
 export class InMemoryGymsRepository implements GymsRepository {
   public gyms: Gym[] = [];
@@ -8,6 +9,22 @@ export class InMemoryGymsRepository implements GymsRepository {
     const gym = await this.gyms.find((gym) => gym.id === id);
 
     if (!gym) return null;
+
+    return gym;
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+      created_at: new Date(),
+    };
+
+    this.gyms.push(gym);
 
     return gym;
   }
