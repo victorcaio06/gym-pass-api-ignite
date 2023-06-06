5cf0ad7cfa56aabb4944e8ac7adfee1628371e7f
Victor Caio <kaioaraujo426@gmail.com>
2023-06-06 09:56:22 -0300

    FEAT: repositories created for Prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,6 @@
-import { prisma } from '@/lib/prisma';
 import { Prisma, User } from '@prisma/client';
+
+import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
