a0eee10076d45f30e2a230504e69ea21cdbcaf5d
Victor Caio <kaioaraujo426@gmail.com>
2023-06-16 14:25:16 -0300

    FEAT: created check-in and added in route

---
+++ src/http/controllers/check-ins/routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify';
+
+import { verifyJwt } from '../../middlewares/verify-jwt';
+import { create } from './create';
+
+export async function checkInsRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt);
+  
+  app.post('/gyms/:gymId/check-ins', create);
+}
