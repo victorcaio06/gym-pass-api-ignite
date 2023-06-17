f80241e4e8732d12c4ff62b3cf63a8d71cea2517
Victor Caio <kaioaraujo426@gmail.com>
2023-06-17 02:30:21 -0300

    TEST: created tests to check-in

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,39 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+import { prisma } from '@/lib/prisma';
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app);
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JV gym',
+        description: 'Some description',
+        phone: '12345678',
+        latitude: -4.9617084,
+        longitude: -39.0189615,
+      },
+    });
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -4.9617084,
+        longitude: -39.0189615,
+      });
+
+    expect(response.statusCode).toEqual(201);
+  });
+});
